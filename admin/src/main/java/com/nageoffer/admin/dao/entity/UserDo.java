8988ed2ce1ba46f8d92ee238af2c0c08074ebbad
Hegra-727 <Hegra727@outlook.com>
2024-01-26 23:07:46 +0800

    feature:Mybatis下划线和实体类驼峰映射

--- admin/src/main/java/com/nageoffer/admin/dao/entity/UserDo.java
+++ admin/src/main/java/com/nageoffer/admin/dao/entity/UserDo.java
@@ -9,11 +9,11 @@ public class UserDo {
     private String id;
     private String username;
     private String password;
-    private String real_name;
+    private String realName;
     private String phone;
     private String mail;
-    private String deletion_time;
-    private String create_time;
-    private String update_time;
-    private String del_flag;
+    private String deletionTime;
+    private String createTime;
+    private String updateTime;
+    private String delFlag;
 }
--- admin/src/main/java/com/nageoffer/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/com/nageoffer/admin/dto/resp/UserRespDTO.java
@@ -1,12 +1,15 @@
 package com.nageoffer.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.nageoffer.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 @Data
 public class UserRespDTO {
     private String id;
     private String username;
-    private String real_name;
+    private String realName;
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
     private String mail;
 }
