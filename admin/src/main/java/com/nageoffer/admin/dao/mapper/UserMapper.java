9948440c2822cc4415b7335463492f054b1c66f1
Hegra-727 <Hegra727@outlook.com>
2024-03-03 10:24:13 +0800

    feature: 规范UserDO类名

--- admin/src/main/java/com/nageoffer/admin/dao/mapper/UserMapper.java
+++ admin/src/main/java/com/nageoffer/admin/dao/mapper/UserMapper.java
@@ -1,11 +1,11 @@
 package com.nageoffer.admin.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.nageoffer.admin.dao.entity.UserDo;
+import com.nageoffer.admin.dao.entity.UserDO;
 
 /**
  * 用户持久层
  */
-public interface UserMapper extends BaseMapper<UserDo> {
+public interface UserMapper extends BaseMapper<UserDO> {
 
 }
