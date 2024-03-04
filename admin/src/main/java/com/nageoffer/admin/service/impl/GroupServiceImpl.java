84e99070e0329724d5918482d44f4ae6dbdecb93
Hegra-727 <Hegra727@outlook.com>
2024-03-04 18:48:14 +0800

    feature: 开发短链接后管系统短链接分组绑定用户上下文

--- admin/src/main/java/com/nageoffer/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nageoffer/admin/service/impl/GroupServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.admin.common.biz.user.UserContext;
 import com.nageoffer.admin.dao.entity.GroupDO;
 import com.nageoffer.admin.dao.mapper.GroupMapper;
 import com.nageoffer.admin.dto.resp.ShortLinkGroupRespDTO;
@@ -21,6 +22,7 @@ import java.util.List;
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
     @Override
     public void saveGroup(String groupName) {
+        System.out.println("上下文用户名username："+UserContext.getUsername());
         String gid;
         do{
             gid= RandomGenerator.generateRandom();
@@ -28,6 +30,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         GroupDO groupDO=GroupDO.builder()
                 .gid(RandomGenerator.generateRandom())
                 .sortOrder(0)
+                .username(UserContext.getUsername())
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
@@ -38,7 +41,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         LambdaQueryWrapper<GroupDO> queryWrapper=Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getDelFlag,0)
                 //TODO 从当前上下文获取用户名
-                .eq(GroupDO::getUsername,"jwl")
+                .eq(GroupDO::getUsername, UserContext.getUsername())
                 .orderByDesc(GroupDO::getSortOrder,GroupDO::getUpdateTime);
         List<GroupDO> groupDOList=baseMapper.selectList(queryWrapper);
         return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
@@ -48,7 +51,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         LambdaQueryWrapper<GroupDO> queryWrapper= Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid,gid)
                 //TODO 设置用户名
-                .eq(GroupDO::getUsername,null);
+                .eq(GroupDO::getUsername,UserContext.getUsername());
         GroupDO hasGroupFlag=baseMapper.selectOne(queryWrapper);
         return hasGroupFlag==null;
     }
