5236220cd43ff1130fd69f5155d23983fc4fd202
Hegra-727 <Hegra727@outlook.com>
2024-03-09 15:05:49 +0800

    feature: 完成短链接后管系统短链接管理的分库分表

--- admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -1,7 +1,7 @@
 package com.nageoffer.shortlink.admin.test;
 
 public class UserTableShardingTest {
-    public static final String SQL="CREATE TABLE `t_user_%d`  (\n" +
+    /*public static final String SQL="CREATE TABLE `t_user_%d`  (\n" +
             "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
             "  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',\n" +
             "  `password` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',\n" +
@@ -14,7 +14,27 @@ public class UserTableShardingTest {
             "  `del_flag` tinyint(1) NULL DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`) USING BTREE,\n" +
             "  UNIQUE INDEX `idx_unique_username`(`username` ASC) USING BTREE\n" +
-            ") ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;";
+            ") ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;";*/
+    public static final String SQL="CREATE TABLE `t_link_%d`  (\n" +
+            "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `domain` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '域名',\n" +
+            "  `short_uri` varchar(8) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '短链接',\n" +
+            "  `full_short_url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '完整短链接',\n" +
+            "  `origin_url` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '原始短链接',\n" +
+            "  `click_num` int NULL DEFAULT 0 COMMENT '点击数',\n" +
+            "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `enable_status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '启用标识0启用1未启用',\n" +
+            "  `created_type` tinyint(1) NULL DEFAULT NULL COMMENT '创建类型0接口创建1控制台创建',\n" +
+            "  `valid_date_type` tinyint(1) NULL DEFAULT NULL COMMENT '有效期类型',\n" +
+            "  `valid_date` datetime NULL DEFAULT NULL COMMENT '有效期',\n" +
+            "  `describe` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '描述',\n" +
+            "  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) NULL DEFAULT NULL COMMENT '删除标识1删除0未删除',\n" +
+            "  PRIMARY KEY (`id`) USING BTREE,\n" +
+            "  UNIQUE INDEX `idx_unique_full_short_url`(`full_short_url` ASC) USING BTREE\n" +
+            ") ENGINE = InnoDB AUTO_INCREMENT = 1766095941448327171 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;\n";
+
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf((SQL)+"%n",i);
